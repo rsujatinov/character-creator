34ab63f2b171110defa71495887fa24c34cc0390
Roman Sujatinov <rsujatinov@pageflex.com>
2023-11-25 23:06:34 +0400

    module work

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
 
 function App() {
-  return (
-    <>
-      <CharacterEditor />
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <div style={{
+                position: "fixed",
+                backgroundColor: "hsl(195deg, 20%, 86%)",
+                bottom: 0,
+                left: 0,
+                right: 0,
+                height: "40%"
+            }}></div>
+            <CharacterEditor />
+            <Footer />
+        </>
+    );
 }
 
 export default App;
